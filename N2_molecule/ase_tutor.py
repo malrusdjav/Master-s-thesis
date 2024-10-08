1bc760bc852159117f3877e7e9e4f6db2425faef
Your Name <you@example.com>
2024-10-08 14:26:00 +0300

    up

---
+++ N2_molecule/ase_tutor.py
@@ -0,0 +1,8 @@
+from ase.visualize import view
+from ase import Atoms
+d = 1.10
+from ase.visualize import view
+molecule = Atoms('N2', positions=[(0., 0., 0.), (0., 0., d)])
+#view(N2, viewer='ngl')
+
+
