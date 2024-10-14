4c7b6934a422c866214c363a1559f6c84c0fc504
Your Name <you@example.com>
2024-10-14 17:22:57 +0300

    u

--- NH3_molecule/ase-nwchem/ase_nwchem_nh3_bfgs.py
+++ NH3_molecule/ase-nwchem/ase_nwchem_nh3_bfgs.py
@@ -4,7 +4,8 @@ from ase.calculators.nwchem import NWChem
 from ase.io import write
 
 nh3 = Atoms('NH3',positions=[[0, 0, 0],[0, 2.4, 2.1],[0, -1.4, 5.1],[0, 3.4, -2.1]])
-nh3.calc = NWChem(xc='PBE')
+nh3.calc = NWChem(dft=dict(iterations=500,
+                       xc='B3LYP'),)
 opt = BFGS(nh3, trajectory='nh3.traj')
 opt.run(fmax=0.02)
 
